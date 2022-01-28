b5de3d57e43e7f28e81b8a437a28aa7de6314a69
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2022-01-28 07:04:02 +0530

    Final deployment changes made

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 import route from "./routes/blogs.js";
-import { password } from "./login.js";
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
-const connectionString = `mongodb+srv://Sayantikag98:${password}@node-tutorial-yt.yawqt.mongodb.net/test`;
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const connectionString = process.env.CONNECTION_URI;
 
 mongoose.connect(connectionString)
 .then(() => {
@@ -24,4 +26,4 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors()); // without this line http request at "localhost:5000/blogs" from "localhost:3000" will be blocked by CORS policy
 //  No 'Access-Control-Allow-Origin' header is present on the requested resource.
 
-app.use("/blogs", route);
+app.use("/", route);
